91700d709862650a89ff48213fa99b1e8663347a
tachittanachote <54389403+tachittanachote@users.noreply.github.com>
2019-10-18 20:46:28 +0700

    created students, teachers, quiz, questions, choices (added students table 2 rows and teachers table 1 row)

--- QuizHub/database/quizhub.sql
+++ QuizHub/database/quizhub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2019 at 05:11 AM
+-- Generation Time: Oct 18, 2019 at 03:42 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -25,34 +25,169 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `quizhub`
+-- Table structure for table `choices`
 --
 
-CREATE TABLE `quizhub` (
-  `quizid` int(255) NOT NULL,
-  `quiz_topic` varchar(255) NOT NULL,
-  `quiz_code` varchar(255) DEFAULT NULL
+CREATE TABLE `choices` (
+  `choice_id` int(255) NOT NULL,
+  `choice_name` varchar(255) NOT NULL,
+  `choice_correct` tinyint(1) NOT NULL,
+  `question_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_id` int(255) NOT NULL,
+  `question_name` varchar(255) NOT NULL,
+  `question_number` varchar(255) NOT NULL,
+  `question_picture` varchar(255) NOT NULL,
+  `quiz_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `quiz_id` int(255) NOT NULL,
+  `quiz_name` varchar(255) NOT NULL,
+  `quiz_comments` varchar(255) NOT NULL,
+  `quiz_status` varchar(255) NOT NULL DEFAULT 'open',
+  `teacher_id` bigint(255) NOT NULL,
+  `course_name` varchar(255) NOT NULL,
+  `course_id` varchar(255) NOT NULL,
+  `join_code` varchar(255) NOT NULL,
+  `cover_images` varchar(255) NOT NULL,
+  `start_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `end_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(255) NOT NULL,
+  `student_id` bigint(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `faculty` varchar(255) NOT NULL,
+  `branch` varchar(255) NOT NULL,
+  `account_status` varchar(255) NOT NULL DEFAULT 'active',
+  `last_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `student_id`, `first_name`, `last_name`, `password`, `faculty`, `branch`, `account_status`, `last_time`) VALUES
+(1, 61130500038, 'demo', 'demo', 'demo', 'School of Information Technology', 'Information Technoloy', 'active', '2019-10-18 13:38:12'),
+(2, 61130500001, 'std01', 'std01', 'std01', 'School of Information Technology', 'Information Technoloy', 'active', '2019-10-18 13:39:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id` int(255) NOT NULL,
+  `teacher_id` bigint(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `faculty` varchar(255) NOT NULL,
+  `course_name` varchar(255) NOT NULL,
+  `course_id` varchar(255) NOT NULL,
+  `account_status` varchar(255) NOT NULL DEFAULT 'pending',
+  `last_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`id`, `teacher_id`, `first_name`, `last_name`, `password`, `faculty`, `course_name`, `course_id`, `account_status`, `last_time`) VALUES
+(1, 100001, 'teacher', 'teacher', 'teacher', 'School of Information Technology', 'Web Programming', 'INT303', 'active', '2019-10-18 13:41:25');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `quizhub`
+-- Indexes for table `choices`
 --
-ALTER TABLE `quizhub`
-  ADD PRIMARY KEY (`quizid`);
+ALTER TABLE `choices`
+  ADD PRIMARY KEY (`choice_id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`quiz_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `quizhub`
+-- AUTO_INCREMENT for table `choices`
 --
-ALTER TABLE `quizhub`
-  MODIFY `quizid` int(255) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `choices`
+  MODIFY `choice_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `question_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `quiz_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
