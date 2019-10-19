bc5f65950262942a7b2144b22555e0b0e32a6575
tachittanachote <54389403+tachittanachote@users.noreply.github.com>
2019-10-19 21:34:58 +0700

    Fixed error sytanx and add new values

--- QuizHub/database/execute_user_account.sql
+++ QuizHub/database/execute_user_account.sql
@@ -1,45 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  tsch
- * Created: Oct 19, 2019
- */
-
-
-//--- Students ----//
-
-INSERT INTO STUDENTS
-	(STUDENT_ID, FIRST_NAME, LAST_NAME, PASSWORD, FACULTY, BRANCH, LAST_TIME)
-VALUES 
-	(61130500001, 'demo', 'demo', 'demo', 'School of Information Technology', 'Information Technology', CURRENT_TIMESTAMP);
 	
 INSERT INTO STUDENTS
-	(STUDENT_ID, FIRST_NAME, LAST_NAME, PASSWORD, FACULTY, BRANCH, LAST_TIME)
+	(STUDENT_ID, FIRST_NAME, LAST_NAME, PASSWORD, FACULTY_ID, BRANCH_ID, LAST_TIME)
 VALUES 
-	(61130500002, 'admin', 'admin', 'admin', 'School of Information Technology', 'Information Technology', CURRENT_TIMESTAMP);
-	
-INSERT INTO STUDENTS
-	(STUDENT_ID, FIRST_NAME, LAST_NAME, PASSWORD, FACULTY, BRANCH, LAST_TIME)
-VALUES 
-	(61130500003, 'ceno', 'ceno', 'ceno', 'School of Information Technology', 'Information Technology', CURRENT_TIMESTAMP);
+	(61130500001, 'Student', 'A', '1234', 2, 1, CURRENT_TIMESTAMP),
+        (61130500002, 'Student', 'B', '1234', 2, 2, CURRENT_TIMESTAMP),
+        (61130500003, 'Student', 'C', '1234', 2, 3, CURRENT_TIMESTAMP);
 
-// ---- Teachers ---- //
-
-INSERT INTO TEACHERS
-	(TEACHER_ID, FIRST_NAME, LAST_NAME, PASSWORD, FACULTY, COURSE_NAME, COURSE_ID, LAST_TIME)
-VALUES 
-	(1000000001, 'John', 'Smith', '1234', 'School of Information Technology', 'Computer Programming', 'INT101', CURRENT_TIMESTAMP);
 	
 INSERT INTO TEACHERS
-	(TEACHER_ID, FIRST_NAME, LAST_NAME, PASSWORD, FACULTY, COURSE_NAME, COURSE_ID, LAST_TIME)
+	(TEACHER_ID, FIRST_NAME, LAST_NAME, PASSWORD, FACULTY_ID, COURSE_NAME, COURSE_ID, LAST_TIME)
 VALUES 
-	(1000000002, 'Sim', 'Gight', '1234', 'School of Information Technology', 'Web Programming', 'INT303', CURRENT_TIMESTAMP);
-	
-INSERT INTO TEACHERS
-	(TEACHER_ID, FIRST_NAME, LAST_NAME, PASSWORD, FACULTY, COURSE_NAME, COURSE_ID, LAST_TIME)
-VALUES 
-	(1000000003, 'Zea', 'Pual', '1234', 'School of Information Technology', 'Network I', 'INT201', CURRENT_TIMESTAMP);
+	(1000000001, 'Teacher', 'A', '1234', 2, 'Network I', 'INT201', CURRENT_TIMESTAMP),
+        (1000000002, 'Teacher', 'B', '1234', 2, 'Network I', 'INT201', CURRENT_TIMESTAMP),
+        (1000000003, 'Teacher', 'C', '1234', 2, 'Network I', 'INT201', CURRENT_TIMESTAMP),
+        (1000000004, 'Teacher', 'D', '1234', 2, 'Network I', 'INT201', CURRENT_TIMESTAMP);
 
