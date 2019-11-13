41515bc633e8c7dd6a1dd6d13214421f444ec0f3
tachittanachote <54389403+tachittanachote@users.noreply.github.com>
2019-11-14 02:19:53 +0700

    Fix 500 Error

--- QuizHub/src/java/servlet/JoinQuizzesServlet.java
+++ QuizHub/src/java/servlet/JoinQuizzesServlet.java
@@ -39,9 +39,11 @@ public class JoinQuizzesServlet extends HttpServlet {
             getServletContext().getRequestDispatcher("/WEB-INF/Home.jsp").forward(request, response);
             getServletContext().getRequestDispatcher("/WEB-INF/Quizzes.jsp").forward(request, response);
         } else {
+            
             Quizdao dq = new Quizdao();
             Quizzes q = dq.findQuizzesByCode(code);
-            if (code == null) {
+            
+            if (q == null) {
                 request.setAttribute("jerror", "Invalid quizzes code.");
                 getServletContext().getRequestDispatcher("/WEB-INF/Home.jsp").forward(request, response);
                 getServletContext().getRequestDispatcher("/WEB-INF/Quizzes.jsp").forward(request, response);
